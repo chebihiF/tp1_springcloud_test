95488c7ad3f423fe3566e7da7eb54521000eb538
chebihiF <chebihi.f@ucd.ac.ma>
2022-12-20 20:32:10 +0100

    init customer-service

---
+++ src/test/java/com/suptech/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suptech.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
